7a7cd97f012e9d4e69d9f2e3292434b6925e6aa9
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2024-05-05 15:12:55 -0400

    Added validation to unique user email

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs'
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
@@ -9,10 +9,22 @@ export class UsersService {
     constructor(private readonly usersRepository: UsersRepository) { }
 
     async create(createUserDto: CreateUserDto) {
+        await this.validateCreateUserDto(createUserDto)
+
         return this.usersRepository.create({
             ...createUserDto,
             password: await bcrypt.hash(createUserDto.password, 10)
         })
+    }
+
+    private async validateCreateUserDto(createUserDto: CreateUserDto) {
+        try {
+            await this.usersRepository.findOne({email: createUserDto.email})
+        } catch (error) {
+            return
+        }
+
+        throw new UnprocessableEntityException('Email already exists.')
     }
 
     async verifyUser(email: string, password: string) {
@@ -20,7 +32,7 @@ export class UsersService {
         const passwordIsValid = await bcrypt.compare(password, user.password)
 
         if (!passwordIsValid) {
-            throw new UnauthorizedException('Credentials are not valid')
+            throw new UnauthorizedException('Credentials are not valid.')
         }
 
         return user
