2bd79a451190d491890b99795e8c805cd72e0c6f
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2024-05-05 18:49:11 -0400

    Added auth microservice

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -4,6 +4,8 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { CurrentUser } from './current-user.decorator';
 import { UserDocument } from './users/models/user.schema';
+import { MessagePattern } from '@nestjs/microservices';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -18,4 +20,8 @@ export class AuthController {
     await this.authService.login(user, response)
     response.send(user)
   }
+
+  @UseGuards(JwtAuthGuard)
+  @MessagePattern('authenticate')
+  async authenticate() {}
 }
--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -3,8 +3,8 @@ import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { UsersService } from "../users/users.service";
-import { Request } from "express";
 import { TokenPayload } from "../interfaces/token-payload.interface";
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromExtractors([
-                (request: Request) => request?.cookies?.Authentication
+                (request: any) => request?.cookies?.Authentication || request?.Authentication
             ]),
             secretOrKey: configService.get('JWT_SECRET')
         })
