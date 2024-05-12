54601520667c7590fe86e366d7a6099cc666b029
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2024-05-11 22:07:19 -0400

    Added nest-cli.json to dockerfile and building only the microservice

--- libs/common/src/dto/create-charge.dto.ts
+++ libs/common/src/dto/create-charge.dto.ts
@@ -1,4 +1,3 @@
-import Stripe from "stripe";
 import { CardDto } from "./card.dto";
 import { IsDefined, IsNotEmptyObject, IsNumber, ValidateNested } from "class-validator";
 import { Type } from "class-transformer";
