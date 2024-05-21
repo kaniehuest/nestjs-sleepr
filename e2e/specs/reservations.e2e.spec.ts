d34988e471d87607327b470c034cb188077f267f
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2024-05-21 18:54:39 -0400

    Refactored tests

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -1,11 +1,13 @@
 describe('Reservations', () => {
+    let jwt: string
+
     beforeAll(async () => {
         const user = {
             email: "e2eUser1@gmail.com",
             password: "str0ngPa$$word!"
         }
 
-        await fetch("http://auth:3001/user", {
+        await fetch("http://auth:3001/users", {
             method: 'POST',
             body: JSON.stringify(user),
             headers: {
@@ -21,11 +23,48 @@ describe('Reservations', () => {
             }
         })
 
-        const jwt  = await response.text()
-        console.log(jwt)
+        jwt = await response.text()
     })
 
-    test('Create', () => {
-        expect(true).toBeTruthy()
+    test('Create and Get', async () => {
+        const createdReservation = await createReservation()
+
+        const responseGet = await fetch(
+            `http://reservations:3000/reservations/${createdReservation._id}`,
+            {
+                headers: {
+                    Authentication: jwt
+                }
+            }
+        )
+        const reservation = await responseGet.json()
+        expect(createdReservation).toEqual(reservation)
     })
+
+    const createReservation = async () => {
+        const responseCreate = await fetch('http://reservations:3000/reservations', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authentication: jwt
+            },
+            body: JSON.stringify({
+                "startDate": "02-01-2024",
+                "endDate": "02-01-2024",
+                "placeId": "123",
+                "invoiceId": "123",
+                "charge": {
+                    "amount": 13,
+                    "card": {
+                        "cvc": "413",
+                        "exp_month": 12,
+                        "exp_year": 2027,
+                        "number": "4242 4242 4242 4242"
+                    }
+                }
+            })
+        })
+        expect(responseCreate.ok).toBeTruthy()
+        return await responseCreate.json()
+    }
 })
