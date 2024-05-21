c4b4afbe44565ef6a72a73be7eac6e122aea163b
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2024-05-21 19:55:59 -0400

    Implemented roles protected routes

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -1,12 +1,19 @@
-import { CanActivate, ExecutionContext, Inject, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from "@nestjs/common";
 import { Observable, catchError, map, of, tap } from "rxjs";
 import { AUTH_SERVICE } from "../constants/services";
 import { ClientProxy } from "@nestjs/microservices";
 import { UserDto } from "../dto";
+import { Reflector } from "@nestjs/core";
+import { Logger } from "@nestjs/common";
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-    constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
+    private readonly logger = new Logger(JwtAuthGuard.name)
+
+    constructor(
+        @Inject(AUTH_SERVICE) private readonly authClient: ClientProxy,
+        private readonly reflector: Reflector
+    ) {}
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const jwt = 
@@ -15,12 +22,28 @@ export class JwtAuthGuard implements CanActivate {
         if (!jwt) {
             return false
         }
+
+        const roles = this.reflector.get<string[]>('roles', context.getHandler())
+
         return this.authClient
         .send<UserDto>('authenticate', { Authentication: jwt})
         .pipe(
-            tap((res) => { context.switchToHttp().getRequest().user = res }),
+            tap((res) => { 
+                if (roles) {
+                    for (const role of roles) {
+                        if (!res.roles?.includes(role)) {
+                            this.logger.error("The user does not have valid roles.")
+                            throw new UnauthorizedException()
+                        }
+                    }
+                }
+                context.switchToHttp().getRequest().user = res 
+            }),
             map(() => true),
-            catchError(() => of(false))
+            catchError((err) => {
+                this.logger.error(err)
+                return of(false)
+            })
         )
     }
 }
