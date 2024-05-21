235010df57bfe8a15910ef0d37df3ef426e4628f
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2024-05-20 22:00:34 -0400

    fix

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -1,18 +1,31 @@
 describe('Reservations', () => {
     beforeAll(async () => {
         const user = {
-            email: "holapistola19@gmail.com",
-            password: "123$uppperPassword"
+            email: "e2eUser1@gmail.com",
+            password: "str0ngPa$$word!"
         }
 
-        await fetch("http://auth:3000", {
+        await fetch("http://auth:3001/user", {
             method: 'POST',
-            body: JSON.stringify(user)
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
 
+        const response = await fetch('http://auth:3001/auth/login', {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const jwt  = await response.text()
+        console.log(jwt)
     })
 
     test('Create', () => {
-
+        expect(true).toBeTruthy()
     })
 })
