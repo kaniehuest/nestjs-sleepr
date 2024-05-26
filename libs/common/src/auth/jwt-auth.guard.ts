dd90b0b81bf508d341431f40f3a782bffe371ffd
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2024-05-26 13:34:27 -0400

    Implemented grpc in jwt auth guard

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -1,24 +1,27 @@
-import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Inject, Injectable, OnModuleInit, UnauthorizedException } from "@nestjs/common";
 import { Observable, catchError, map, of, tap } from "rxjs";
-import { AUTH_SERVICE } from "../constants/services";
-import { ClientProxy } from "@nestjs/microservices";
-import { User } from "../models";
+import { ClientGrpc } from "@nestjs/microservices";
 import { Reflector } from "@nestjs/core";
 import { Logger } from "@nestjs/common";
-import { Role } from "../models";
+import { AUTH_SERVICE_NAME, AuthServiceClient } from "../types";
 
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
+export class JwtAuthGuard implements CanActivate, OnModuleInit {
     private readonly logger = new Logger(JwtAuthGuard.name)
+    private authService: AuthServiceClient
 
     constructor(
-        @Inject(AUTH_SERVICE) private readonly authClient: ClientProxy,
+        @Inject(AUTH_SERVICE_NAME) private readonly client: ClientGrpc,
         private readonly reflector: Reflector
-    ) {}
+    ) { }
+
+    onModuleInit() {
+        this.authService = this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME)
+    }
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const jwt = 
-            context.switchToHttp().getRequest().cookies?.Authentication || 
+        const jwt =
+            context.switchToHttp().getRequest().cookies?.Authentication ||
             context.switchToHttp().getRequest().headers?.authentication
         if (!jwt) {
             return false
@@ -26,25 +29,28 @@ export class JwtAuthGuard implements CanActivate {
 
         const roles = this.reflector.get<string[]>('roles', context.getHandler())
 
-        return this.authClient
-        .send<User>('authenticate', { Authentication: jwt})
-        .pipe(
-            tap((res) => { 
-                if (roles) {
-                    for (const role of roles) {
-                        if (!res.roles?.map((role: Role) => role.name).includes(role)) {
-                            this.logger.error("The user does not have valid roles.")
-                            throw new UnauthorizedException()
+        return this.authService
+            .authenticate({ Authentication: jwt })
+            .pipe(
+                tap((res) => {
+                    if (roles) {
+                        for (const role of roles) {
+                            if (!res.roles?.map((role: any) => role.name).includes(role)) {
+                                this.logger.error("The user does not have valid roles.")
+                                throw new UnauthorizedException()
+                            }
                         }
                     }
-                }
-                context.switchToHttp().getRequest().user = res 
-            }),
-            map(() => true),
-            catchError((err) => {
-                this.logger.error(err)
-                return of(false)
-            })
-        )
+                    context.switchToHttp().getRequest().user = {
+                        ...res,
+                        _id: res.id
+                    }
+                }),
+                map(() => true),
+                catchError((err) => {
+                    this.logger.error(err)
+                    return of(false)
+                })
+            )
     }
 }
